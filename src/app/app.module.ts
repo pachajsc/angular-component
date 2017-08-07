13025a30f2aae7d7bf44349393b3a75cef638181
Jose S Cardoso <jose.segundo.cardoso@accenture.com>
2017-08-06 21:03:17 -0300

    create component table and tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TabsComponent } from '../../src/app/library/tabs/tabs.component';
+import { TableComponent } from '../../src/app/library/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule
